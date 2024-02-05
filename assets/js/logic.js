43b5035124a1a012b8c493e6089b620a02af5e62
Obioma <obeeomaa@gmail.com>
2024-02-05 17:16:19 +0100

    Add references to elements

---
+++ assets/js/logic.js
@@ -0,0 +1,12 @@
+// Reference elements
+const startScreen = document.getElementById("start-screen");
+const endScreen = document.getElementById("end-screen");
+const feedback = document.getElementById("feedback");
+const choicesEl = document.getElementById("choices");
+const questionScreen = document.getElementById("question-screen");
+const questionTitle = document.getElementById("question-title");
+const finalScore = document.getElementById("final-score");
+const startQuizButton = document.getElementById("start");
+const submitButton = document.getElementById("submit");
+const timerEl = document.getElementById("timer");
+let time = document.getElementById("time");
