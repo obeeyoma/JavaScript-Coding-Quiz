6794da985da414f0a29bb61568b6770ae19ca010
Obioma <obeeomaa@gmail.com>
2024-02-05 17:32:05 +0100

    Add array of question objects

---
+++ assets/js/questions.js
@@ -0,0 +1,40 @@
+// Array of question objects to store question data
+const questions = [
+  {
+    title: "Which of the following is NOT a JavaScript data type?",
+    choices: ["1. Number", "2. String", "3. Undefined", "4. Collection"],
+    answer: "4. Collection",
+  },
+  {
+    title:
+      "Which function is used to serialize an object into a JSON string in Javascript?",
+    choices: [
+      "1. convert()",
+      "2. stringify()",
+      "3. parse()",
+      "4. None of the above.",
+    ],
+    answer: "2. stringify()",
+  },
+  {
+    title: "How do you stop an interval timer in Javascript?",
+    choices: [
+      "1. intervalOver",
+      "2. clearTimer",
+      "3. clearInterval",
+      "4. hideTimer",
+    ],
+    answer: "3. clearInterval",
+  },
+  {
+    title: "Which one is NOT a comparison operator?",
+    choices: ["1. < ", "2. = ", "3. > ", "4. != "],
+    answer: "2. = ",
+  },
+  {
+    title:
+      "Which of the following attribute is used to include external js code inside your HTML?",
+    choices: ["1. class ", "2. href ", "3. link ", "4. src "],
+    answer: "4. src ",
+  },
+];
